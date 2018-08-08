ba5e8ab247cfb54fd00bcfa52384cdd54d87c014
ddd-sch <35425386+ddd-sch@users.noreply.github.com>
2018-08-08 23:36:18 +0200

    Kommentare

--- iot_raq10a/iot_raq10a.ino
+++ iot_raq10a/iot_raq10a.ino
@@ -1,8 +1,10 @@
+//Includes for OLED-Display
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_FeatherOLED.h>
 
+//Include our classes
 #include "RAQ_BME280.h"
 #include "RAQ_BME680.h"
 #include "RAQ_CCS811.h"
@@ -10,14 +12,17 @@
 #include "RAQ_HttpClient.h"
 #include "RAQ_SensorData.h"
 
+//Name of RoomAirQuality-Sensor
 #define SENSOR_ID "EU02"
 
+//Define WiFi SSID and PASS
 #define WIFI_SSID "DESKTOP-B7JQT0N 4039"
 #define WIFI_PASS "27Q?c465"
 
+//Global vars
 Adafruit_SSD1306 oled = Adafruit_SSD1306();
 
-RAQ_BME280 sensor_bme;                  //change type to RAQ_BME280 if combo board is connected
+RAQ_BME280 sensor_bme;                  //change type to RAQ_BME680 if combo board is connected
 RAQ_CCS811 sensor_ccs;
 RAQ_Wifi   wifi(WIFI_SSID, WIFI_PASS);
 RAQ_HttpClient http("192.168.137.1", 9000);
@@ -25,7 +30,6 @@ RAQ_HttpClient http("192.168.137.1", 9000);
 unsigned long millis_wifi = 0;
 unsigned long millis_displaySensorData = 0;
 unsigned long millis_addSensorData = 0;
-
 
 SensorDataList sensorDataList;
 SensorData currentSensorData;
@@ -37,6 +41,7 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  
   delay(10);  //Make sure sensors had enough time to turn on.
 
   //Initialize oled-display
@@ -59,23 +64,29 @@ void setup() {
   int status_bme = sensor_bme.init();
   int status_ccs = sensor_ccs.init();
 
+  //Stop if problem with Sensors
   if (!status_bme || !status_ccs) {
     while(true);
   }
 
+  //Connect with Hotspot
   wifi.connect();
 
+  //Wait for WiFi to connect
   delay(5000);
 }
 
 void loop() {
+  //If WiFi is not connected, try every 5sec
   if (wifi.getWifiStatus() != WL_CONNECTED &&
       millis() - millis_wifi >= 5000) {
     wifi.connect();
     millis_wifi = millis();
   }
   
+  //Get Sensordata every 1.5sec and print on Serial/OLED
   if (millis() - millis_displaySensorData >= 1500) {
+    //Get Sensordata and save into struct
     currentSensorData = {
       millis(), 
       sensor_bme.getTemp(),
@@ -120,6 +131,7 @@ void loop() {
     millis_displaySensorData = millis();
   }
 
+  //Add Sensordata every 5sec to list
   if (millis() - millis_addSensorData >= 5000) {
     SensorData* temp = new SensorData{
         currentSensorData.timestamp, 
@@ -130,19 +142,30 @@ void loop() {
         currentSensorData.tvoc,
         NULL
       };
+    
     bool addRes = sensorDataList.addSensorData(temp, true);
+    
+    //test.....
     if (addRes) {
       Serial.println("Added SensorData to List.");
     } else {
       Serial.println("Failed to add SensorData to List.");
     }
+    
     millis_addSensorData = millis();
   }
 
+  //If WiFi is connected, send Sensordata from List to Server
   if (wifi.getWifiStatus() == WL_CONNECTED) {
+    //Get Sensordata from List
     SensorData* tempSensorData = sensorDataList.getSensorData();
+    
+    //If List is not mt
     if (tempSensorData != NULL) {
+      
       Serial.println("Prepare to send Data.");
+      
+      //Make JSON-String (HTTP Request Body)
       String reqBody = "[{\"SensorID\":\"" + String(SENSOR_ID)
         + "\",\"Zeit\":" + String(millis() - tempSensorData->timestamp)
         + ",\"Temperatur\":" + String(tempSensorData->temp)
@@ -151,14 +174,19 @@ void loop() {
         + ",\"CO2\":" + String(tempSensorData->co2)
         + ",\"TVOC\":" + String(tempSensorData->tvoc)
         + "}]";
+      
+      //Print for debug
       Serial.println(reqBody);
+      
+      //Send Data
       bool res = http.postJSON("/", reqBody);
+      
       if (res) {
         Serial.println("Sent Data.");
-        delete tempSensorData;
+        delete tempSensorData;      //Deallocate Memory
       } else {
         Serial.println("Failed to send Data.");
-        sensorDataList.addSensorData(tempSensorData, false);
+        sensorDataList.addSensorData(tempSensorData, false);    //Put back into List
       }
     }
   }
